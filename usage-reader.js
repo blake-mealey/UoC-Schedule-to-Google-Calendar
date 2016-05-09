ac7658251830e941766d9fdb6ad7cc5c46f1692a
Blake <blakemealey@gmail.com>
2016-05-09 11:30:32 -0600

    Added usage-reader script to interpret usage db

---
+++ usage-reader.js
@@ -0,0 +1,74 @@
+// get libraries
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
+// declare constants
+var dbURL = "mongodb://localhost:27017/schedule-app";
+
+// get arguments
+var outputFile = process.argv[2];
+if(outputFile == null) { outputFile = "out.csv"; }
+
+console.log("Starting usage reader.");
+
+// read data from database
+function getData(callback) {
+	MongoClient.connect(dbURL, function(err, db) {
+		if(err != null) { console.log("Error connecting to database: " + err); return; }
+
+		console.log("Reading data from database.");
+
+		var cursor = db.collection("usage").find();
+		var data = []
+
+		cursor.each(function(err, doc) {
+			if(err != null) { console.log("Error reading data from database: " + err); return; }
+
+			if(doc != null) {
+				var usage = {
+					date: new Date(doc.date.year, doc.date.month, doc.date.day, doc.date.hour, doc.date.minute).toLocaleDateString(),
+					calendarName: doc.calendarname,
+					courses: []
+				}
+
+				for(var i = doc.courseData.length - 1; i >= 0; i--) {
+					var courseData = doc.courseData[i];
+					usage.courses[usage.courses.length] = {
+						courseName: courseData.classInfo.name,
+						instructor: courseData.prof
+					}
+				}
+
+				data[data.length] = usage;
+			} else {
+				db.close();
+				callback(data);
+			}
+		})
+	});
+}
+
+// write data to .csv file
+getData(function(data) {
+	console.log("Saving data to .csv file.");
+
+	var formattedData = "";
+	for(var i = data.length - 1; i >= 0; i--) {				// TODO: write simple library to make this easier :P
+		var usage = data[i];
+		formattedData += usage.date + ",";
+		formattedData += usage.calendarName + "\n";
+		for(var j = 0; j < usage.courses.length; j++) {
+			var course = usage.courses[j];
+			formattedData += ((j + 1) + ",");
+			formattedData += course.courseName + ","
+			formattedData += course.instructor + "\n"
+		}
+		formattedData += "\n"
+	}
+
+	fs.writeFile(outputFile, formattedData, function(err) {
+		if(err) { return console.log("Error writing to .csv file: " + err); }
+
+		console.log("Data saved to " + outputFile);
+	});
+});
